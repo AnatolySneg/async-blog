c9f468287c82071c91f94cd81ff4b03cc5cb14e8
Anatolii Sniehurov <anatolii.sniehurov@paloalto-group.com>
2026-05-12 00:46:46 +0300

    set up FastAPI application with auth and blog routers

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from async-blog!")
+from fastapi import FastAPI
+from api.routers.auth import auth_router
+from api.routers.blog import blog_router
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="", debug=True, version="0.1.0")
+
+app.include_router(auth_router)
+app.include_router(blog_router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to async_blog! Be polite and respectful to other members."}
+
