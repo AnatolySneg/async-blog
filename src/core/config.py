c69fe5e1663947a6d7fb6518cf8e4cc316964c21
Anatolii Sniehurov <anatolii.sniehurov@paloalto-group.com>
2026-05-25 17:03:27 +0300

    update config: adjust database settings and environment handling
    - Renamed `POSTGRES_DB` to `POSTGRES_DATABASE` for consistency.
    - Added `BASE_DIR` to dynamically load `.env` file path.
    - Updated `.env_template` with new structure and removed unused `DATABASE_URL`.

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,11 @@
+import os
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+
+
+# Project root directory
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 class Settings(BaseSettings):
@@ -8,17 +14,20 @@ class Settings(BaseSettings):
     Currently retrieves data from environment variables (with priority) and from the .env file.
     In the future, it will be easy to add YAML or other sources to model_config.
     """
+    # General settings
+    PROJECT_NAME: str = "FastAPI Async-blog"
+    DEBUG: bool = Field(True, description="Whether the app is running in production mode")
 
     # Database settings with type validation
     POSTGRES_USER: str = Field(..., description="Database user")
     POSTGRES_PASSWORD: str = Field(..., description="Database password")
-    POSTGRES_DB: str = Field(..., description="Database name")
+    POSTGRES_DATABASE: str = Field(..., description="Database name")
     POSTGRES_HOST: str = Field(..., description="Database host")
     POSTGRES_PORT: int = Field(5432, description="Database port")
 
     # Configuration for where pydantic will retrieve data from
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=os.path.join(BASE_DIR, ".env"),
         env_file_encoding="utf-8",
         # Ignore any extra variables in the .env file
         # that are not described in this class
@@ -28,6 +37,6 @@ class Settings(BaseSettings):
     @property
     def database_url(self) -> str:
         """Constructs a URL for SQLAlchemy connection based on basic parameters."""
-        return f"database+databa://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}"
 
 settings = Settings()
