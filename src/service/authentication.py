7b1bead64974e0238012d10942a753f597ff8db3
Anatolii Sniehurov <anatolii.sniehurov@paloalto-group.com>
2026-05-29 17:14:01 +0300

    feat(services): implement base methods in AuthenticationService with user repository integration

--- src/service/authentication.py
+++ src/service/authentication.py
@@ -1,4 +1,17 @@
-
+from src.models.user import User
 
 class AuthenticationService:
-    pass
+    def __init__(self, user_repo):
+        self.user_repo = user_repo
+
+    async def get_user_by_id(self, user_id: int) -> User:
+        return await self.user_repo.get_by_id(user_id)
+
+    async def register(self, email: str, password: str) -> User:
+        pass # TODO: Implement user registration
+
+    async def login(self, email: str, password: str) -> User:
+        pass # TODO: Implement user login
+
+
+
