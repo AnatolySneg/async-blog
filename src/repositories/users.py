cb5bcc45589d6573630274ba74b829982d4babc4
Anatolii Sniehurov <anatolii.sniehurov@paloalto-group.com>
2026-05-29 13:22:22 +0300

    feat(repositories): implement and expand UserRepository with email lookup and user creation methods

---
+++ src/repositories/users.py
@@ -0,0 +1,36 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.models.user import User
+
+
+class UserRepository:
+    pass
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_email(self, email: str) -> User | None:
+        """
+        Retrieves a user instance from the database based on the provided email address.
+
+        :param email: The email address of the user to retrieve.
+        :type email: str
+        :return: The user instance if a user is found, otherwise None.
+        :rtype: User | None
+        """
+        stmt = select(User).where(User.email == email)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
+
+    async def create(self, **user_data) -> User:
+        new_user = User(**user_data)
+        self.session.add(new_user)
+        try:
+            await self.session.commit()
+            await self.session.refresh(new_user)
+            return new_user
+        except Exception as e:
+            await self.session.rollback()
+            #TODO: log error and raise http exception
+            #raise
+            return None
