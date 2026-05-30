135967ba58739cae74625e1443098f74895ea5be
Anatolii Sniehurov <anatolii.sniehurov@paloalto-group.com>
2026-05-31 01:51:57 +0300

    feat(api): enhance auth endpoints with request validation, response models, and service integration

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+
+from src.validation.auth import RegistrationResponse
+from src.validation.base import StandardResponse
+from src.validation import UserRegisterSchema
+from src.service.authentication import AuthenticationService
+from src.api.dependencies import ServiceManager
+from src.utils.response_handler import ApiResponse
 
 auth_router = APIRouter()
 
-@auth_router.post("/auth")
+@auth_router.post("/auth", response_model=StandardResponse)
 async def auth():
     return {"message": "Hello World"}
 
-@auth_router.post("/register")
-async def register():
-    return {"message": "Registered"}
+@auth_router.post("/register", response_model=StandardResponse[RegistrationResponse])
+async def register(user_data: UserRegisterSchema, service: AuthenticationService = Depends(ServiceManager.get_auth_service)):
+    try:
+        result = await service.register_user(user_data.model_dump())
+        return ApiResponse.success(data=result, message="User registered successfully.", status_code=status.HTTP_201_CREATED)
+    except ValueError as e:
+        return ApiResponse.failure(status_code=status.HTTP_400_BAD_REQUEST, message="Registration failed.", detail=str(e))
 
 @auth_router.post("/logout")
 async def logout():
